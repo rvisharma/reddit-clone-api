0dcf84c59ad780090b8d01da548ed6bfd605f85d
Ravi Sharma <rvisharma91@gmail.com>
2017-02-05 00:27:41 +0530

    env

--- server/app.js
+++ server/app.js
@@ -2,9 +2,11 @@ import express from 'express';
 import routes from './routes';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import mLab from '../env';
 
-mongoose.connect('mongodb://rvi:newpassword@ds141209.mlab.com:41209/cms', () => {
-  console.log('Connected to MLab!..');
+
+mongoose.connect(`mongodb://${mLab.username}:${mLab.password}@ds141209.mlab.com:41209/cms`, () => {
+  console.log('Connected to MLab!');
 })
 
 const app = express();
